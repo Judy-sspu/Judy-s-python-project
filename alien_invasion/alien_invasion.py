78d77cc184d3a4bf4b4d31d051d7b240d5842701
Judy-sspu <61716615+Judy-sspu@users.noreply.github.com>
2024-03-18 23:24:43 +0800

    Update alien_invasion.py

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,12 +2,13 @@ import sys
 
 import pygame
 
-
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
     screen = pygame.display.set_mode((1200,800))
     pygame.display.set_caption("Alien Invasion")
+
+    bg_color = (230,230,230)
 
     # 开始游戏的主循环
     while True:
@@ -16,6 +17,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # 每次循环时都重绘屏幕
+        screen.fill(bg_color)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
